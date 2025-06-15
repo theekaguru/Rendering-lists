a0386adc8ca15174f0eea69738b80505f75d4fce
theekaguru <danielkaguru2001@gmail.com>
2025-06-15 17:54:44 +0300

    keys and filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import './App.css'
+// import './App.css'
 import { Joke } from './components/Joke'
 import jokesData from "./data/data.json"
 
@@ -8,11 +8,21 @@ function App() {
 
   return (
     <>
-     {
+     {/* {
       jokesData.map((joke)=>(
         <Joke joke ={joke.joke} rating={joke.rating}/>
       ))
+     } */
+     
+
+      //filter and display jokes with >3
+
+      jokesData.filter((joke) =>joke.rating > 3).map((joke) =>(
+        <Joke key={joke.id} joke ={joke.joke} rating={joke.rating}/>
+      ))
      }
+
+     
     </>
   )
 }
--- src/components/Joke.tsx
+++ src/components/Joke.tsx
@@ -1,9 +1,26 @@
 import "./jokes.scss"
 export const Joke = ({joke , rating } :{joke:string , rating:number}) => {
-  return (
+ const ratingfunction = (rating: number) => {
+    if (rating === 1) {
+        return <span>★☆☆☆☆</span>
+    }else if (rating === 2){
+        return <span>★★☆☆☆</span>
+    }else if (rating === 3){
+        return <span>★★★☆☆</span>
+    }else if (rating === 4){
+        return <span>★★★★☆</span>
+    }else if (rating === 5){
+        return <span>★★★★★</span>
+    }
+    return <span>☆☆☆☆☆</span>
+
+        
+    
+ }
+    return (
     <div className="joke">
         <h4 className="jokename">{joke}</h4>
-        <p className="rating">{rating}</p>
+        <p className="rating">{ratingfunction(rating)}</p>
     </div>
   )
 }
